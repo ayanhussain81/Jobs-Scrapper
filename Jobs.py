807a81c720b2822eb5cf7623f3d6387d88e1cbe8
Ayan Hussain <90856741+ayanhussain81@users.noreply.github.com>
2023-09-04 15:41:48 +0500

    Update Jobs.py

--- Jobs.py
+++ Jobs.py
@@ -13,43 +13,81 @@ from webdriver_manager.chrome import ChromeDriverManager
 from datetime import datetime
 from selenium.common.exceptions import TimeoutException
 
+options =Options()
+driver= webdriver.Chrome()
+driver.get('https://arbetsformedlingen.se/platsbanken/annonser?q=energibranschen')
+main_links  = ['https://arbetsformedlingen.se/platsbanken/annonser?q=energibranschen' , 'https://arbetsformedlingen.se/platsbanken/annonser?q=energiteknik', 'https://arbetsformedlingen.se/platsbanken/annonser?q=energisystem']
 
-options = Options()
-options.add_argument('--headless')
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-
-driver.get('https://arbetsformedlingen.se/platsbanken/annonser?q=energi')
-time.sleep(10)
-try:
-    element = driver.find_element(By.XPATH, "/html/body/div[1]/div[2]/div[7]/div/div/main/div[3]/div/div/div[2]/div/div/div/div/div[2]/div[2]/pb-root/div/pb-page-search/div[2]/pb-feature-tabs/div[2]/pb-section-search-result/div/div/div/div/div/div/pb-feature-pagination/digi-navigation-pagination/div/nav/div/ol/li[6]/digi-button/button/span/span")
-    inner_text = element.get_attribute("innerText")
-    page_number = int(inner_text)
-    print("Extracted integer:", page_number)
-except:
-    print("Unable to convert inner text to integer")
-    page_number = 50
-
+count = 0 
 all_links = []
-for num in range(page_number):
-    url = 'https://arbetsformedlingen.se/platsbanken/annonser?q=energi&page=' + str(num + 1)
-    driver.get(url)
-    count = 0
-    while True:
-        count = count +1 
-        time.sleep(2)
-        links = driver.find_elements(By.XPATH, "//a[contains(@href, '/platsbanken/annonser/')]")
-        if links:
-            break  
-        if count == 15:
-            break
-        print("No links found. Retrying...")
-    
-    for link in links:
-        job_link = link.get_attribute('href')
-        all_links.append(job_link)
+for link in main_links:
+    count  = count + 1 
+    page_number = 0 
+    driver.get(link)
+    time.sleep(8)
+    click =  driver.find_element(By.CLASS_NAME,'search-criteria-holder.ng-star-inserted').click()
+    time.sleep(1)
+    # Find all buttons with the specified class names
+    buttons = driver.find_elements(By.CLASS_NAME,'digi-button.digi-button--secondary.digi-button--m.sc-digi-button')
+
+    # Get the text from the last button in the list
+    if buttons:
+        page_number = buttons[-2].text
+        print("Text of the last button:", page_number)
+    else:
+        print("No matching buttons found on the page")
+        if count == 1:
+            page_number = 6 
+        else:
+            page_number = 2
+    page_number = int(page_number)
+    print(page_number)
+
+    for num in range(page_number):
+        url = 'https://arbetsformedlingen.se/platsbanken/annonser?q=energi&page=' + str(num + 1)
+        driver.get(url)
+        count = 0
+        while True:
+            count = count +1 
+            time.sleep(2)
+            links = driver.find_elements(By.XPATH, "//a[contains(@href, '/platsbanken/annonser/')]")
+            if links:
+                break  
+            if count == 15:
+                break
+            print("No links found. Retrying...")
+
+        for link in links:
+            job_link = link.get_attribute('href')
+            all_links.append(job_link)
+all_links = sorted(all_links)
+def extract_date(input_string):
+    pattern = r'\b(?:\d{1,2}\s+)?(?:januari|februari|mars|april|maj|juni|juli|augusti|september|September|oktober|november|december|\d{1,2})\s+\d{2}(?=\.\d{2})\b'
+
+    date_matches = re.findall(pattern, input_string, re.IGNORECASE)
+
+    if date_matches:
+        # Convert the month names to month numbers
+        month_mapping = {
+            'januari': '01', 'februari': '02', 'mars': '03', 'april': '04',
+            'maj': '05', 'juni': '06', 'juli': '07', 'augusti': '08',
+            'september': '09','September': '09', 'oktober': '10', 'november': '11', 'december': '12'
+        }
         
+        extracted_date = date_matches[0]
+        parts = extracted_date.split()
+        
+        # If the day is not present, use '01' as default
+        day = parts[0] if parts[0].isdigit() else '01'
+        month = month_mapping.get(parts[1].lower(), '01') if parts[1].lower() in month_mapping else '01'
+        year = '2023'
+        
+        formatted_date = f'{year}-{month}-{day}'
+        return formatted_date
+    else:
+        return None
+    
+    
 def is_id_present(api_url, target_id):
     response = requests.get(api_url)
     
@@ -111,7 +149,7 @@ def scrape_job_data(row):
         WebDriverWait(driver, 20).until(EC.presence_of_element_located(( By.XPATH, "/html/body/div[1]/div[2]/div[7]/div/div/main/div[3]/div/div/div[2]/div/div/div/div/div[2]/div[2]/pb-root/div/pb-page-job/div/section/div/div[2]/div[2]/section/pb-section-job-main-content")))                               
         about_job = driver.find_element(By.XPATH, "/html/body/div[1]/div[2]/div[7]/div/div/main/div[3]/div/div/div[2]/div/div/div/div/div[2]/div[2]/pb-root/div/pb-page-job/div/section/div/div[2]/div[2]/section/pb-section-job-main-content")
         about_job_html = about_job.get_attribute("outerHTML")
-        extra = '<a href="Source URL" class="button">Ansök på arbetsförmedlingen</a>'
+        extra = f'<a href="{row}" class="button">Ansök på arbetsförmedlingen</a>'
         about_job_html = about_job_html + extra
     except:
         about_job_html =''
@@ -129,21 +167,27 @@ def scrape_job_data(row):
     except:
         address=''
     WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.XPATH,'//*[@id="pb-root"]/pb-page-job/div/section/div/div[2]/div[2]/aside[1]/div/pb-section-job-apply-component/div/div/div[1]/strong')))                               
-    last_date= driver.find_element(By.XPATH,'//*[@id="pb-root"]/pb-page-job/div/section/div/div[2]/div[2]/aside[1]/div/pb-section-job-apply-component/div/div/div[1]/strong').text
-    fixed_year = "2023"
+    last_date1= driver.find_element(By.XPATH,'//*[@id="pb-root"]/pb-page-job/div/section/div/div[2]/div[2]/aside[1]/div/pb-section-job-apply-component/div/div/div[1]/strong').text
+    last_date = extract_date(last_date1)
+    count = 0
+    while last_date == None:
+        count = count +1 
+        last_date = extract_date(last_date1)
+        if count == 10:
+            break
+        print(last_date)
 
-    # Define a dictionary to map Swedish month names to numerical values
-    month_mapping = {
-        'januari': '01', 'februari': '02', 'mars': '03', 'april': '04',
-        'maj': '05', 'juni': '06', 'juli': '07', 'augusti': '08',
-        'september': '09', 'oktober': '10', 'november': '11', 'december': '12'
-    }
-    # Split the input string into parts
-    parts = last_date.split()
+    if last_date == None:
+        try:
+            last_date = extract_date(last_date1)
+        except:
+            input_format = "%d %B %H:%M"
+            fixed_year = 2023
+            parsed_date = datetime.strptime(last_date1, input_format)
+            parsed_date = parsed_date.replace(year=fixed_year)
+            output_format = "%Y-%m-%d"
+            last_date = parsed_date.strftime(output_format)
 
-    day = parts[0]
-    month = month_mapping.get(parts[1].lower(), '01')  
-    last_date = f'{fixed_year}-{month}-{day}'
     print(last_date)
 
     job_info = {
@@ -171,25 +215,24 @@ def scrape_job_data(row):
 api_url_get = "https://aktuellenergi.se/wp-json/wpjobopenings/v1/job-openings"
 api_url_post = "https://aktuellenergi.se/wp-json/wpjobopenings/v1/add-job"
 
- 
+i = 0 
 for row in all_links:
-    try:
-        job_info = scrape_job_data(row)
-        print(job_info)
-        target_id = row.split('/')[-1]
+    i = i + 1
+    print(i)
+    job_info = scrape_job_data(row)
+    print(job_info)
+    target_id = row.split('/')[-1]
+    if i == 10:
+        break
+    if is_id_present(api_url_get, target_id):
+        print(f"ID {target_id} is present in the data.")
+    else:
+        print(f"ID {target_id} is not present in the data.")
+        response = requests.post(api_url_post, json=job_info)
 
-        if is_id_present(api_url_get, target_id):
-            print(f"ID {target_id} is present in the data.")
+        # Check the response status
+        if response.status_code == 200:
+            print("Job posted successfully!")
         else:
-            print(f"ID {target_id} is not present in the data.")
-            response = requests.post(api_url_post, json=job_info)
-
-            # Check the response status
-            if response.status_code == 200:
-                print("Job posted successfully!")
-            else:
-                print("Failed to post job.")
-                print("Response:", response.text)
-    except:
-        print('passs')
-        pass
+            print("Failed to post job.")
+            print("Response:", response.text)
